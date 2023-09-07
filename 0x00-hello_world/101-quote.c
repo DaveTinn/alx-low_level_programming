bbaafd93eea4e963af0fbe1e27faaa4612cef99d
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-07 15:54:15 -0700

    C-program that prints exactly a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: if 1 is Success
+ */
+int main(void)
+{
+if("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
