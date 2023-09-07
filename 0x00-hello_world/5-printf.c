0b5811ff2ca876b07260c0313350edb79883baf5
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-07 13:21:51 -0700

    A C program that prints exactly using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry pointi
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("%s",with proper grammar,but the outcome is a piece of art);
+return (0);
+}	
