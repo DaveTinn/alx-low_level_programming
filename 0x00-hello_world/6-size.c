7ce284766081531c224562f5d7d0d963207248d1
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-07 14:13:14 -0700

    A C-program printing the size of various types on computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%zu", sizeof char);
+printf("zu", sizeof int);
+printf("zu", sizeof long int);
+printf("zu", sizeof long long int);
+printf("zu", sizeof float);
+return (0);
+}
