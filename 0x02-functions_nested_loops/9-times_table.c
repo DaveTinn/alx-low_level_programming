60385dcae6b0073a638bafa4aca5e4a08256381f
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-12 20:45:04 -0700

    A function printing 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,31 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Entry point
+ * times_table- Function printing multiplication table.
  *
  * Return: Always 0
  */
+void times_table(void)
+{
+	int d;
+	int e;
+	int multiply;
+
+	for (d = 0; d < 10; d++)
+	{
+		for (e = 0; e < 10; e++)
+		{
+			multiply = d * e;
+			if (e == 0)
+				printf("%d, ", multiply);
+			else
+			{
+				printf("%2d", multiply);
+				if (e != 0)
+					printf(", ");
+			}
+
+		}
+		printtf("\n");
+	}
+
+}
