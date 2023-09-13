22cbef273aac0064cbfff887a7868a748f235683
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-13 08:28:14 -0700

    A function printing the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,7 @@ void times_table(void)
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
+			}
 			if (e < 9)
 			{
 				_putchar(',');
@@ -35,5 +36,4 @@ void times_table(void)
 			}
 		}
 	}
-
 }
