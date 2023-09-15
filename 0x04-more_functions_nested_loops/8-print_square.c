1b60be4105518f4f1d59ed7459efc5812f1801fd
codedave <ayodeji_oloruntoba@yahoo.com>
2023-09-15 09:23:00 -0700

    A function printing a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,27 @@
 #include "main.h"
 /**
- * print_square(int size)
+ * print_square - Function to be used.
+ * @size: Parameter to be checked.
  *
+ * Return: 0 Always
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int d = 0;
+		int e = 0;
+
+		for (d = 0; d < size; d++)
+		{
+			for (e = 0; e < size; e++)
+			{
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
