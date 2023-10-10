d55e65912db0a31af65c49a57e6b324b088d8025
codedave <ayodeji_oloruntoba@yahoo.com>
2023-10-10 16:05:10 -0700

    A function defining a new type 'struct dog'

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure to be defined
+ * @name: parameter of char data type
+ * @age: parameter of float data type
+ * @owner: parameter of char data type
+ * Return: 0 Always
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
