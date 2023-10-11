1f4cb7a34c80b51e52bda1d687f7706b6ef0a7be
codedave <ayodeji_oloruntoba@yahoo.com>
2023-10-11 12:45:53 -0700

    A function printing the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to check code
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	int num_Bytes;
+	int d;
+	unsigned char opcode;
+	int (*hexa_lower)(int, char **) = main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_Bytes = atoi(argv[1]);
+	if (num_Bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (d = 0; d < num_Bytes; d++)
+	{
+		opcode = *(unsigned char *)hexa_lower;
+		printf("%.2x", opcode);
+		if (d == num_Bytes - 1)
+			continue;
+		printf(" ");
+		hexa_lower++;
+	}
+	printf("\n");
+	return (0);
+}
