cea323087009e534913aa43d8841cecbe00b808d
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-10-19 17:19:03 -0700

    A function that prints a text

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-
+#include "lists.h"
+/**
+ * print_text - prints out a sentence
+ *
+ * Return: 0 Always
+ */
+void print_text(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
