613ce035097f9bf660f68e061e08ebb11ca307d8
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-10-26 18:23:13 -0700

    A function returning the number of bits to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
  * flip_bits - function to flib bits
  * @n: parameter of int type
  * @m: parameter of int type
- * 
+ *
  * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
