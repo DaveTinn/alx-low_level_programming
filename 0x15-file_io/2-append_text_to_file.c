3840062c783d914f18d633732d7447dfaee4138a
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-10-30 18:40:38 -0700

    A function appending text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,8 +22,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len];len++)
+			;
 		file_text = write(app_text, text_content, len);
 		if (file_text == -1)
 		{
