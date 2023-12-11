8269f24327e4bfabd62ba093c48790b9f4a02064
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-12-11 14:10:15 -0800

    A dynamic library containing C functions called by Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * Addition - sums two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: addition of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * Subtraction - subtracts two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: differences of the two integers
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * Multiplication - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: multiplication of the two integers
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * Division - divides two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: division of the two integers
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * Modulus - remains two integers after division
+ * @a: integer
+ * @b: integer
+ *
+ * Return: remainder of the two integers
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
