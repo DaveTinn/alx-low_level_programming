de344cfb35bf8dd3c8011a88b473c274792407c7
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-12-22 07:24:08 -0800

    A function deleting a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,5 +7,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        hash_table_t *del_ht;
+	hash_node_t *next;
+	unsigned long int idx;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+	{
+		return;
+	}
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		while (ht->array[idx] != NULL)
+		{
+			next = ht->array[idx]->next;
+			free(ht->array[idx]->key);
+			free(ht->array[idx]->value);
+			free(ht->array[idx]);
+			ht->array[idx] = next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
