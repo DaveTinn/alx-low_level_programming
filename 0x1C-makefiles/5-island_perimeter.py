19dd5e05acdccc799b647b0401d9c50157962a2b
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-21 15:24:34 -0800

    Implemented the island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,4 +4,18 @@
 
 def island_perimeter(grid):
     """Returns the prerimeter of the island described."""
-    grid = []
+    width = len(grid[0])
+    height = len(grid)
+    island_edges = 0
+    size_of_island = 0
+
+    for idx in range(height):
+        for i in range(width):
+            if grid[idx][i] == 1:
+                size_of_island += 1
+                if (i > 0 and grid[idx][i - 1] == 1):
+                    island_edges += 1
+                if (idx > 0 and grid[idx - 1][i] == 1):
+                    island_edges += 1
+
+    return size_of_island * 4 - island_edges * 2
